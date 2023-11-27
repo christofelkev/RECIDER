df1587b7bca792d75183089a7501c087cf971a64
Christofel Kevin <116001020+christofelkev@users.noreply.github.com>
2023-11-27 13:55:14 +0700

    add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,16 @@ android {
 
 dependencies {
 
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+    //lifecycle
+    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
+
+    //Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
